ee3604ca628f5f5ead5261cda7384003afe7e9d0
masalehh <ma.saleh007@gmail.com>
2026-01-03 11:55:57 +0600

    Graph dfs traverse implemneted using python

--- Graph/dfs_traverse.py
+++ Graph/dfs_traverse.py
@@ -1,7 +1,11 @@
-def dfs_traverse(start, graph):
-    visited = set() 
+from typing import  List, Dict, TypeVar, Set 
+
+T = TypeVar('T')
+
+def dfs_traverse(start: T, graph: Dict[T, List[T]]) -> None: 
+    visited: Set[T] = set() 
     
-    def dfs_helper(node):
+    def dfs_helper(node: T) -> None:
         visited.add(node)
         print(node)
         
