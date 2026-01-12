d1b4beadbe9b92e9482884390e619c8e8f5fed8f
masalehh <ma.saleh007@gmail.com>
2026-01-12 19:05:28 +0600

    implement preorder traversal of a tree iterative way

--- Tree/pre_order_iterative.py
+++ Tree/pre_order_iterative.py
@@ -1,6 +1,6 @@
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
-        self.val = val
+        self.value = val
         self.left = left
         self.right = right
 
@@ -16,9 +16,18 @@ def preorder_iterative(root):
         node = stack.pop() 
         result.append(node.value)
         
-        if node.right:
+        if node.right:                  # Right must be processed after left, thats why we pusdhed right to stack first 
             stack.append(node.right)
             
         if node.left:
-            stack.append(node.left)
+            stack.append(node.left)     # left must be processed before right thats why we pushed it last as stack follows LIFO 
     return result 
+
+"""
+      1
+     / \
+    2   3
+   / \
+  4   5
+
+"""
