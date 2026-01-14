dbe842429eb6beac19adc1420cf6cd40753fa46d
masalehh <ma.saleh007@gmail.com>
2026-01-14 18:38:32 +0600

    Build tree using level order from list of values

---
+++ Tree/Build_tree_lever_order.py
@@ -0,0 +1,56 @@
+from collections import deque 
+
+
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None) -> None:
+        self.value = value
+        self.left = left
+        self.right = right 
+        
+        
+def build_tree(values):
+    if not values or values[0] is None:
+        return None 
+    
+    root = TreeNode(values[0]) 
+    queue = deque([root]) 
+    i = 1 
+    
+    while queue and i < len(values):
+        node = queue.popleft() 
+        
+        if i < len(values) and values[i] is not None:
+            node.left = TreeNode(values[i]) 
+            queue.append(node.left) 
+        i += 1 
+        if i < len(values) and values[i] is not None:
+            node.right = TreeNode(values[i])
+            queue.append(node.right) 
+        i += 1 
+        
+        
+    return root 
+         
+            
+"""
+You are given below list to build a tree: 
+[1, 2, 3, None, 4, 5, 6]
+output tree will looks like: 
+        1
+       / \
+      2   3
+       \  / \
+        4 5  6
+        
+        
+🔹 Main Loop (Important Part)
+while queue and i < len(values):
+
+
+✔ Continue while:
+
+There are nodes waiting to get children
+
+We still have values to process
+
+"""
